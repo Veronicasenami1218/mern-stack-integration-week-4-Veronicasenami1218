02ebc300a77dcfd7a29eaee9891aea12b2c2dc2c
Veronica Tankpinu <veronicasenami977@gmail.com>
2025-11-17 11:35:18 +0100

    Updated src/App.jsx to expose /login and /signup and Wrap dashboard and progress routes with ProtectedRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
-// Auth pages temporarily disabled per requirement to show dashboard directly
-// import Login from './pages/Login'
-// import Signup from './pages/Signup'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import ProgressList from './pages/ProgressList'
 import ProgressForm from './pages/ProgressForm'
@@ -11,10 +11,33 @@ import ProgressForm from './pages/ProgressForm'
 export default function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route element={<Layout />}>        
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/progress" element={<ProgressList />} />
-        <Route path="/progress/new" element={<ProgressForm />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/progress"
+          element={
+            <ProtectedRoute>
+              <ProgressList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/progress/new"
+          element={
+            <ProtectedRoute>
+              <ProgressForm />
+            </ProtectedRoute>
+          }
+        />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
